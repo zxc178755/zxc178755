5547eb39f55e798f893432c5918812cccb86101f
zxc <zxc178755@163.com>
2021-09-17 20:19:11 +0800

    firstcommit

---
+++ src/main/java/CosSimilarity.java
@@ -0,0 +1,77 @@
+import com.huaban.analysis.jieba.JiebaSegmenter;
+import com.huaban.analysis.jieba.SegToken;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CosSimilarity {
+    protected Map<String,int[]> wordMap;
+    public CosSimilarity(){
+        wordMap=new HashMap<>();
+    }
+
+    /**
+     *
+     */
+    protected void textSegment(List<String> Article, int index){
+        JiebaSegmenter segmenter = new JiebaSegmenter();
+        List<SegToken> segTokenList;
+        for(String sentence : Article){
+
+            segTokenList=segmenter.process(sentence,JiebaSegmenter.SegMode.INDEX);
+            putWordtoMap(segTokenList,index);
+        }
+    }
+
+    protected void putWordtoMap(List<SegToken> segTokenList,int index){
+        for(SegToken segToken: segTokenList){
+            String word =segToken.word;
+            if(wordMap.containsKey(word)){
+                wordMap.get(word)[index]++;
+            }else{
+                int[] count =new int[2];
+                count[0]= index==0?1:0;
+                count[1]= index==1?1:0;
+                wordMap.put(word,count);
+            }
+        }
+    }
+
+    protected double calculateCos()
+    {
+        double top = calculateTop();
+        double bottom = calculateBottom();
+        return top/bottom;
+    }
+
+    protected double calculateTop()
+    {
+        double top = 0.0;
+        for(int[] count : wordMap.values())
+            top += count[0]*count[1];
+        return top;
+    }
+
+    protected double calculateBottom()
+    {
+        double num1=0.0;
+        double num2=0.0;
+        double sqrt1 =0.0;
+        double sqrt2=0.0;
+        for(int [] count : wordMap.values())
+        {
+            num1 += count[0]*count[0];
+            num2 += count[1]*count[1];
+        }
+        sqrt1 = Math.sqrt(num1);
+        sqrt2 = Math.sqrt(num2);
+        return sqrt1*sqrt2;
+    }
+    public void finish()
+    {
+        wordMap = null;
+    }
+}
+
+
---
+++ src/main/java/TextSimilarity.java
@@ -0,0 +1,101 @@
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+class TextCheck {
+    List<String> Article1;//原文章
+    List<String> Article2;//对比文章
+    private String path1; //原文章目录
+    private String path2;//对比文章目录
+    private String outputPath;//输出文件目录
+    private static final Logger logger = Logger.getLogger("TextCheck");
+
+    public TextCheck(String path1,String path2,String outputPath){
+        Article1=new ArrayList<>();
+        Article2=new ArrayList<>();
+        this.path1= path1;
+        this.path2= path2;
+        this.outputPath= outputPath;
+
+        /**
+         * 把文章存放到ArrayList中
+         */
+        AddList(path1,Article1);
+        AddList(path2,Article2);
+
+        CosSimilarity cos=new CosSimilarity();
+        cos.textSegment(Article1,0);
+        cos.textSegment(Article2,1);
+
+
+        /**
+         * 输出文件
+         */
+        OutputFile(Similarity(cos.calculateCos()));
+
+    }
+
+    private void AddList(String path,List<String> list){
+        try(FileInputStream fis = new FileInputStream(path);
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);){
+
+            String line;
+            /*
+             * 读取单行文本
+             * */
+            while ((line=br.readLine())!=null){
+                line = FilterSentence(line);
+                if(!line.isEmpty()){
+                    list.add(line);
+                }
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();//抛出异常
+        }
+    }
+
+    /**
+     * 过滤字符串
+     * @param sentence 需要过滤字符串
+     * @return 过滤后
+     */
+    protected String FilterSentence(String sentence){
+        sentence =sentence.trim();//过滤前后空格
+        return sentence;
+    }
+
+    /**
+     * 得到相似度冰取小数点后两位
+     * @return
+     */
+    public String Similarity(Double cos){
+        return "0"+new DecimalFormat("#.000").format(cos).substring(0,3);
+//        return null;
+    }
+
+    protected void OutputFile(String similarity){
+        File outPutFile = new File(outputPath);
+        try(FileOutputStream fos = new FileOutputStream(outPutFile);
+            PrintWriter pw = new PrintWriter(fos);)
+        {
+            pw.write("源文件："+path1+"\r\n");
+            pw.write("对比文件："+path2+"\r\n");
+            pw.write("文本相似度："+similarity);
+            pw.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TextCheck("testArticle/orig.txt", "testArticle/orig_0.8_add.txt","testArticle/orig_output2.txt");
+    }
+}
